b9151f32d5798312d0b9c6174617c87e506e4d7c
lylla834 <123461723+lylla834@users.noreply.github.com>
2023-06-16 12:27:14 +0200

    Update providers.tf

--- staging_environment/providers.tf
+++ staging_environment/providers.tf
@@ -11,7 +11,7 @@ provider "azurerm" {
    #client_secret = var.client_secret
    tenant_id = var.tenant_id
    skip_provider_registration = true
-   features {
+   feature {
   - name = "Microsoft.ContainerInstance"
  }
 }
